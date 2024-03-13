b026cde15d404353420853c39b2449f1866e3f15
57899182 <sourcegd@users.noreply.github.com>
2024-03-14 00:49:36 +0100

    implem ElemDex ( reste a gérer le dégrisage )

--- app/src/main/java/com/example/fusionbolt/ElemDexFragment.java
+++ app/src/main/java/com/example/fusionbolt/ElemDexFragment.java
@@ -1,13 +1,27 @@
 package com.example.fusionbolt;
-
+import android.os.Bundle;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.viewmodel.CreationExtras;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class ElemDexFragment extends Fragment {
-    @NonNull
+
+    public static ElemDexFragment newInstance() {
+        return new ElemDexFragment();
+    }
+
     @Override
-    public CreationExtras getDefaultViewModelCreationExtras() {
-        return super.getDefaultViewModelCreationExtras();
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_elemdex, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
